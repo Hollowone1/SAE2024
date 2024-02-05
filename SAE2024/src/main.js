5eb799ea4a5fdfa1122f094f22fd9fffca2aa805
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-05 16:15:56 +0100

    bullshit job

--- SAE2024/src/main.js
+++ SAE2024/src/main.js
@@ -1,17 +1,11 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { LMap, LTileLayer, LMarker } from 'leaflet';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.component('l-map', LMap);
-app.component('l-tile-layer', LTileLayer);
-app.component('l-marker', LMarker);
-
 
 app.use(createPinia())
 app.use(router)
