529c31af533400c751d0153862887613e1d3e602
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-05 12:06:43 +0100

    accueil done

---
+++ SAE2024/src/views/Accueil.vue
@@ -0,0 +1,25 @@
+<template>
+
+<button RouterLink to="./NouvellePartie" type="submit"> Nouvelle Partie</button>
+<button RouterLink to="./Replay" type="submit"> Rejouer une partie</button>
+<button RouterLink to="./Profil" type="submit"> Mon profil </button>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap');
+
+
+button{
+    font-family: 'Protest Riot', sans-serif;
+    background-color: #42B480;
+    color: #ffffff;
+}
+
+</style>
