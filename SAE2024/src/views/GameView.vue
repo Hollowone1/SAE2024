2ec542f7907481db331bedc7bb8dea380c4f54d1
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-05 16:29:11 +0100

    push maps

--- SAE2024/src/views/GameView.vue
+++ SAE2024/src/views/GameView.vue
@@ -1,37 +1,59 @@
 <template>
- <h2> Map View</h2>
- <div style="height:600px; width:800px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></l-tile-layer>
-    </l-map>
-  </div>
-</template>
-
-<script>
-import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-
-export default{
-  components: {
-    LMap,
-    LTileLayer,
-  },
-  data() {
-    return {
-      zoom: 2,
-    };
-  },
-};
-
-</script>
-
-
-
-<style>
-
-</style>
-
+    <div>
+      <h2>Map View</h2>
+      <div style="height: 600px; width: 800px">
+        <l-map ref="map" v-model:zoom="zoom" :center="center" @contextmenu="updateMarker">
+          <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+          ></l-tile-layer>
+          <l-geo-json :geojson="geoJson"></l-geo-json>
+        </l-map>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import "leaflet/dist/leaflet.css";
+  import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
+  
+  export default {
+    components: {
+      LMap,
+      LTileLayer,
+      LGeoJson,
+    },
+    data() {
+      return {
+        zoom: 2,
+        center: [47.41322, -1.219482],
+        geoJson: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [0, 0],
+              },
+              properties: {
+                title: "Marker",
+                icon: "circle",
+              },
+            },
+          ],
+        },
+      };
+    },
+    methods: {
+      updateMarker(event) {
+        const latLng = event.latlng;
+        this.geoJson.features[0].geometry.coordinates = [latLng.lng, latLng.lat];
+      },
+    },
+  };
+  </script>
+  
+  <style></style>
+  
