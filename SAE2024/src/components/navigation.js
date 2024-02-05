3db653ebe54f0116176eb19c9be5d9c1a4d369e5
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-05 12:18:34 +0100

    pushed navigation component

--- SAE2024/src/components/navigation.js
+++
@@ -1,8 +0,0 @@
-<template>
- <nav>
-    <routerLink to="../views/NouvellePartie.view">Nouvelle Partie</routerLink>
-    <h1>Géo-Quizz</h1>
-    <routerLink to="../views/Profil.view">Mon profil</routerLink>
-
- </nav>
-</template>
---
+++ SAE2024/src/components/navigation.vue
@@ -0,0 +1,27 @@
+<template>
+ <nav>
+    <routerLink to="../views/NouvellePartie.view">Nouvelle Partie</routerLink>
+    <h1>Géo-Quizz</h1>
+    <routerLink to="../views/Profil.view">Mon profil</routerLink>
+ </nav>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap');
+
+nav{
+    display: 'flex';
+    
+}    
+
+a{
+    justify-content: 'space-around';
+    text-decoration: none;
+    font-family: 'Protest Riot', sans-serif;
+}
+
+</style>
