5951a4183b756f4362d4192f0d367a860d56e534
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-06 09:49:30 +0100

    vif logic

--- SAE2024/src/components/navigation.vue
+++ SAE2024/src/components/navigation.vue
@@ -2,13 +2,17 @@
  <nav>
     <routerLink to="/nouvelle-partie">Nouvelle Partie</routerLink>
     <routerLink to="/"><h1>Géo-Quizz</h1></routerLink>
-    <routerLink to="../views/Profil.view">Mon profil</routerLink>
+    <router-link v-if="utilisateurConnecte" to="/profil">Mon profil</router-link>
  </nav>
 </template>
 
 <script>
-    export {
-
+    export default {
+        data() {
+        return {
+            utilisateurConnecte: false,
+        };
+    },
     }
 </script>
 <style>
