7ed22a3ae16662df022c8e759de3238f6463ce32
BJulOs <julesbresson88600@gmail.com>
2024-02-06 10:45:55 +0100

    Register comme connexion, n'est pas fonctionnel mais bon début

---
+++ SAE2024/src/components/Register.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="register-container">
+    <h1>Inscrivez-vous</h1>
+    <p>Pour commencer à jouer</p>
+    <form @submit.prevent="submitForm">
+      <label for="name">Nom</label>
+      <input v-model="name" type="text" id="name" name="name" placeholder="Nom">
+      <br>
+      <label for="email">Email</label>
+      <input v-model="email" type="email" id="email" name="email" placeholder="Email">
+      <br>
+      <label for="password">Mot de passe</label>
+      <input v-model="password" type="password" id="password" name="password" placeholder="Mot de passe">
+      <br>
+      <router-link type="submit" to="/"><button class="inscription">Inscription</button></router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('register', this.name, this.email, this.password)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1.5em;
+  margin-bottom: 10px;
+  text-align: left;
+}
+
+p {
+  margin-top: 0;
+  text-align: left;
+}
+.register-container {
+  background-color: #28293E; /* couleur de fond du reste du site */
+  color: #ffffff; /* texte en blanc */
+  border-radius: 10px; /* bord arrondis */
+  border: 3px solid;
+  border-color: #717394;
+  padding: 10rem 5rem;
+  width: 300px;
+  text-align: initial;
+  margin: auto;
+}
+</style>
