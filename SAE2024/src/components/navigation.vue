1cb344dc30bd55d7c3663847ec253114cb4cf74a
BJulOs <julesbresson88600@gmail.com>
2024-02-06 09:58:02 +0100

    Pas de style

--- SAE2024/src/components/navigation.vue
+++ SAE2024/src/components/navigation.vue
@@ -19,37 +19,3 @@
     },
     }
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap");
-
-nav {
-  background-color: #28293E;
-  padding: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-a {
-  font-family: "Protest Riot", sans-serif;
-  text-decoration: none;
-  color: #fff;
-  margin: 0 15px;
-  transition: color 0.3s;
-}
-
-a:hover {
-  color: #42B480;
-}
-html {
-  margin: 0;
-  padding: 0;
-}
-
-h1 {
-  font-family: "Protest Riot", sans-serif;
-  color: #fff;
-  margin: 0;
-}
-</style>
