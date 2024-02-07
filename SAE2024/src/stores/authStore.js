a9626fe23562d774cb2ac42c061e4ac1cd3f91dd
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-07 15:08:31 +0100

    big au modfi

--- SAE2024/src/stores/authStore.js
+++ SAE2024/src/stores/authStore.js
@@ -7,41 +7,49 @@ export const useAuthStore = defineStore('auth', {
     errorMessage: null,
   }),
 
-  baseUrlSignUp: 'http://localhost:2082/api/users/signup',
-
-  baseUrlProfil: 'http://localhost:2082/api/users/profile',
-
-  actions: {
-    async authenticate({ username, password }) {
-        
-      try {
-        const baseUrlSignIn =  'http://localhost:2082/api/users/signin' 
-        const response = await baseUrlSignIn.authenticate(username, password);
-
-        if (response.success) {
-          this.isAuthenticated = true;
-          this.user = response.user;
-          this.errorMessage = null;
-        } else {
+    actions: {
+      async authenticate({ username, password }) {
+        try {
+          const baseUrlSignIn = 'http://localhost:2082/api/users/signin';
+          const response = await fetch(baseUrlSignIn, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+          });
+  
+          const data = await response.json();
+  
+          if (data.success) {
+            this.isAuthenticated = true;
+            this.user = data.user;
+            const jwtToken = data.token;
+            this.token = jwtToken;
+            this.errorMessage = null;
+          } else {
+            this.isAuthenticated = false;
+            this.user = null;
+            this.errorMessage = data.message;
+          }
+        } catch (error) {
+          console.error('Erreur lors de l\'authentification', error);
           this.isAuthenticated = false;
           this.user = null;
-          this.errorMessage = response.message;
+          this.errorMessage = 'Une erreur s\'est produite lors de l\'authentification';
         }
-      } catch (error) {
-        console.error('Erreur lors de l\'authentification', error);
+      },
+  
+      logout() {
         this.isAuthenticated = false;
         this.user = null;
-        this.errorMessage = 'Une erreur s\'est produite lors de l\'authentification';
-      }
+        this.errorMessage = null;
+  
+      
+      },
     },
-
-    logout() {
-      this.isAuthenticated = false;
-      this.user = null;
-      this.errorMessage = null;
-    },
-  },
-});
+  })
+  
 
 export const useRegisterStore = defineStore('user', {
     state: () => ({
