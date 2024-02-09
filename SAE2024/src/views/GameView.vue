a4ca464eac89a7f6294e3d5654e52247edf1ef3b
tamourgh1u <grindmoney19@gmail.com>
2024-02-09 11:29:33 +0100

    gameview

--- SAE2024/src/views/GameView.vue
+++ SAE2024/src/views/GameView.vue
@@ -5,83 +5,138 @@
       <img src="../assets/a2c7fc86c6b887959f61fd704ff9d8c2bbc1c34f774d3dc41654207db787be9d.webp">
       <div class="mapstyle">
         <l-map ref="map" v-model:zoom="zoom" :center="center" :max-zoom="maxZoom" :min-zoom="minZoom" :zoom-control="false" :useGlobalLeaflet="false" @click="placeMarker">
-          <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap"></l-tile-layer>
-          <l-marker :lat-lng="markerLatLng" :draggable="true" @dragend="onMarkerDragEnd" @click="checkDistance"></l-marker>
+          <l-tile-layer url="https://tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=b90e089c365242a3a08dbb49a7084a61" layer-type="base" name="OpenStreetMap"></l-tile-layer>
+          <l-marker :lat-lng="markerLatLng" :draggable="true" @dragend="onMarkerDragEnd" @click="checkDistance">
+            <l-popup :content="popupContent" />
+          </l-marker>
         </l-map>
       </div>
+
+    </div>
+    <div class="score-container">
+      
+      <table class="points-table">
+        <thead>
+          <tr>
+            <th>Essais Restants</th>
+            <th>Score</th>
+            <th>Meilleure Distance</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{remainingAttempts}}</td>
+            <td>{{score}}</td>
+            <td>{{ bestDistance }} km</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-if="gameOver">
+      <p>Fin de la partie !</p>
+      <p>Score final : {{ score }}</p>
+      <p>Meilleure distance : {{ bestDistance }} km</p>
+      <button @click="resetGame">Recommencer</button>
     </div>
   </div>
-</template>
+  </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import {LMap, LMarker, LTileLayer} from "@vue-leaflet/vue-leaflet";
+import L from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 
 export default {
+  name: "GameView",
   components: {
     LMap,
     LTileLayer,
     LMarker,
+    LPopup,
   },
   data() {
     return {
-      zoom: 18,
+      imageUrl: "../assets/a2c7fc86c6b887959f61fd704ff9d8c2bbc1c34f774d3dc41654207db787be9d.webp",
+      zoom: 12,
       center: [48.6921, 6.1844],
       markerLatLng: [48.6921, 6.1844],
       maxZoom: 25,
       minZoom: 1,
       targetLocation: { lat: 48.6921, lon: 6.1844 },
-      distanceParameter: 5,
+      distanceParameter: 0.1,
+      popupContent: "",
+      score: 0,
+      bestDistance: 100,
+      totalAttempts: 4,
+      remainingAttempts: 4,
+      gameOver: false,
     };
   },
   methods: {
-    calculateDistance(lat1, lon1, lat2, lon2) {
-      const R = 6371; // Rayon moyen de la Terre en kilomètres
-      const dLat = this.toRad(lat2 - lat1);
-      const dLon = this.toRad(lon2 - lon1);
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      return R * c;
-    },
-    toRad(degrees) {
-      return degrees * Math.PI / 180;
-    },
-    checkDistance() {
-      const distance = this.calculateDistance(
-        this.targetLocation.lat,
-        this.targetLocation.lon,
-        this.markerLatLng[0], // Latitude du marqueur
-        this.markerLatLng[1]  // Longitude du marqueur
-      );
+      checkDistance() {
+        const markerLatLng = L.latLng(this.markerLatLng[0], this.markerLatLng[1]);
+        const targetLatLng = L.latLng(this.targetLocation.lat, this.targetLocation.lon);
+        const distance = markerLatLng.distanceTo(targetLatLng) / 1000; 
+        console.log(distance); 
 
-      if (distance < this.distanceParameter) {
-        console.log("5 points - Excellent guess!");
-      } else if (distance < 2 * this.distanceParameter) { //TODO - calcul non fonctionnel
-        console.log("3 points - Good guess!");
-      } else if (distance < 5 * this.distanceParameter) { //TODO - calcul non fonctionnel
-        console.log("1 point - Close enough!");
+        let points = 0;
+        if (distance < this.distanceParameter) {
+          points = 5;
+        } else if (distance < 3 * this.distanceParameter) {
+          points = 3;
+        } else if (distance < 4 * this.distanceParameter) {
+          points = 2;
+        } else {
+          points = 0;
+        }
+
+        this.popupContent = `Vous avez gagné ${points} points !`;
+
+        this.score += points;
+        if (distance < this.bestDistance) {
+          this.bestDistance = distance;
+        }
+        if (this.remainingAttempts > 0) {
+        this.popupContent = `Vous avez gagné ${points} points !`;
+        this.score += points;
+        if (distance < this.bestDistance) {
+          this.bestDistance = distance;
+        }
+
+        this.remainingAttempts--;
+        if (this.remainingAttempts === 0) {
+          this.gameOver = true;
+        }
       } else {
-        console.log("No points - Try again!");
+        this.popupContent = "Vous n'avez plus d'essais restants.";
       }
+
+      },
+      placeMarker(event) {
+        this.markerLatLng = [event.latlng.lat, event.latlng.lng];
+      },
+      onMarkerDragEnd(event) {
+        this.markerLatLng = [event.target.getLatLng().lat, event.target.getLatLng().lng];
+      },
+      resetGame() {
+      this.score = 0;
+      this.bestDistance = 100;
+      this.remainingAttempts = this.totalAttempts;
+      this.gameOver = false;
+      this.markerLatLng = [this.center[0], this.center[1]];
     },
-    placeMarker(event) {
-      this.markerLatLng = event.latlng;
     },
-    onMarkerDragEnd(event) {
-      this.markerLatLng = event.target.getLatLng();
-    },
-  },
-};
+  };
 </script>
 
+
 <style scoped>
-.mapstyle{
-  position: relative;
-}
-map{
-  position: absolute;
-}
+    .mapstyle{
+      position: relative;
+    }
+    map{
+      position: absolute;
+    }
+
 </style>
